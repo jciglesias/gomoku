754cc4d3055ba4ec1b06b3bb411fc87c8d3fc3b7
jciglesias <jiglesia@student.42.fr>
2025-06-12 18:13:21 +0200

    add player vs player mode

--- src/game.py
+++ src/game.py
@@ -11,6 +11,7 @@ def change_board_size():
         del st.session_state.bot_time
 
 with st.sidebar:
+    mode = st.radio("Game Mode", ["Player vs Player", "Player vs Bot"], horizontal=True, key="mode")
     board_size = st.slider("Board Size", 5, 20, 19, 1, on_change=change_board_size, key="board_size")
     win_len = st.slider("Winning Length", 3, 10, 5, 1)
 
@@ -29,9 +30,9 @@ if st.session_state.current_player == -1 and 'bot_time' in st.session_state:
 for i in range(board_size):
     cols = st.columns(board_size)
     for j in range(board_size):
-        if cols[j].button(marks[st.session_state.board[i][j]], key=f"{i}-{j}", disabled=st.session_state.current_player == 1):
+        if cols[j].button(marks[st.session_state.board[i][j]], key=f"{i}-{j}", disabled=st.session_state.current_player == 1 and mode == "Player vs Bot"):
             if check_valid_move(st.session_state.board, i, j, 0, st.session_state.current_player):
-                st.session_state.board = make_move(st.session_state.board, i, j, -1, 0)
+                st.session_state.board = make_move(st.session_state.board, i, j, st.session_state.current_player, 0)
             else:
                 st.toast("Invalid move! Please try again.", icon="🚫")
                 continue
@@ -50,7 +51,7 @@ for i in range(board_size):
             if 'bot_time' in st.session_state:
                 del st.session_state.bot_time
             st.rerun()
-if st.session_state.current_player == 1:
+if st.session_state.current_player == 1 and mode == "Player vs Bot":
     start_time = perf_counter()
     st.session_state.board = bot_move(st.session_state.board, board_size, win_len, st.session_state.last_move)
     end_time = perf_counter()
