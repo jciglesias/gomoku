8b76ee9aa336d1aef35febc9452b87c08ad402a5
Alban KERLOC'H <alban.kerloh@gmail.fr>
2025-06-12 19:48:08 +0200

    check capture OK

--- src/heuristic.py
+++ src/heuristic.py
@@ -96,12 +96,6 @@ def heuristic(board_size, win_len, tab, player):
     reward_closed[win_len] = 10**(win_len)
     reward_open1[win_len] = 10**(win_len)
     reward = [reward_closed, reward_open1, reward_open2]
-
-    # reward = [[0, 0, 0, 0, 0, 100000], #closed
-    #         [0, 0, 0.1, 1, 100, 100000], #open 1
-    #         [0, 0, 0.5, 10, 1000, 100000] #open 2
-    #         ]
-
     tab = np.array(tab)
     res = heuristic_row(board_size, win_len, tab, player, reward)
     res += heuristic_row(board_size, win_len, tab.transpose(), player, reward)
--- src/valid_move.py
+++ src/valid_move.py
@@ -1,3 +1,13 @@
+directions = [(-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+            (0, 1),
+            (1, -1),
+            (1, 0),
+            (1, 1)
+            ]
+
 def check_threes(board, row, col, empty_cell, player):
     directions = [(1, 0), (0, 1), (1, 1), (1, -1)]
     threes = 0
@@ -14,8 +24,27 @@ def check_threes(board, row, col, empty_cell, player):
                 threes += 1
     return threes >= 2
 
+def check_capture(board, row, col, empty_cell, player):
+    for i in range(8):
+        r = row  + directions[i][0]
+        c = col + directions[i][1]
+        if 0 <= r < len(board) and 0 <= c < len(board):
+                if board[r][c] == -player:
+                        r = row - directions[i][0]
+                        c = col - directions[i][1]
+                        if 0 <= r < len(board) and 0 <= c < len(board):
+                                if board[r][c] == player:
+                                        r = row  - directions[i][0] * 2
+                                        c = col - directions[i][1] * 2
+                                        if 0 <= r < len(board) and 0 <= c < len(board):
+                                                if board[r][c] == -player:
+                                                        return True
+    return False
+
 def check_valid_move(board, row, col, empty_cell, player):
     if board[row][col] != empty_cell:
+        return False
+    if check_capture(board, row, col, empty_cell, player):
         return False
     # check if the move creates a double-three
     if check_threes(board, row, col, empty_cell, player):
