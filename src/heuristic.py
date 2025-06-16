f76e6796df0d9e00c92b415a49efe8e165c9d0bc
Alban KERLOC'H <alban.kerloh@gmail.fr>
2025-06-16 17:01:42 +0200

    get lines in heuristic

--- src/heuristic.py
+++ src/heuristic.py
@@ -2,30 +2,55 @@ import pandas as pd
 import numpy as np
 import time
 
-directions = [(-1, -1),
-            (-1, 0),
-            (-1, 1),
-            (0, -1),
-            (0, 1),
-            (1, -1),
+directions = [(0, 1),
             (1, 0),
-            (1, 1)
+            (1, 1),
+            (-1, 1)
             ]
 
-def heuristic(board_size, win_len, tab, player, row, col):
+def getlines(board_size, win_len, tab, player, row, col):
+    lignes = []
+    for i in range(win_len - 1):
+        ligne = ''  
+        for j in range(-(win_len - 1), win_len):
+            r = row + j * directions[i][0]
+            c = col + j * directions[i][1]
+            if 0 <= r < board_size and 0 <= c < board_size:
+                if tab[r][c] == 0:
+                    ligne += '_'
+                elif tab[r][c] == player:
+                    ligne += 'X'
+                else:
+                    ligne += 'O'
+        lignes.append(ligne)
+    return lignes
+
+def extract_motifs(ligne):
     res = 0
-    for i in range(8):
-        r = row  + directions[i][0]
-        c = col + directions[i][1]
-        if 0 <= r < board_size and 0 <= c < board_size:
-            if tab[r][c] == -player: # check capture
-                r = row - directions[i][0]
-                c = col - directions[i][1]
-                if 0 <= r < board_size and 0 <= c < board_size:
-                    if tab[r][c] == -player:
-                        r = row  - directions[i][0] * 2
-                        c = col - directions[i][1] * 2
-                        if 0 <= r < board_size and 0 <= c < board_size:
-                            if tab[r][c] == player:
-                                res += 10 # reward capture
+    pattern_scores = [
+        ('XXXXX', 100000),   # 5
+        ('_XXXX', 10000),    # 4 opened
+        ('XXXX_', 10000),    # 4 opened
+        ('XXXX', 5000),      # 4 blocked
+        ('_XXX', 1000),    # 3 opened
+        ('XXX_', 1000),    # 3 opened
+        ('XXX', 100),      # 3 blocked
+        ('XX', 50),      # 3 blocked
+        ('X', 10),      # 3 blocked
+    ]
+    for pattern, score in pattern_scores:
+        index = 0
+        while index < len(ligne):
+            if ligne[index:].startswith(pattern):
+                res += score
+                index += 1
+            else:
+                index += 1
+    return res
+
+def heuristic(board_size, win_len, tab, player, row, col):
+    lignes = getlines(board_size, 5, tab, player, row, col)
+    res = 0
+    for ligne in lignes:
+        res += extract_motifs(ligne)
     return res
