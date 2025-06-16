e540ab2f17cde85979c8a51fc34287dd08ee83f8
Alban KERLOC'H <alban.kerloh@gmail.fr>
2025-06-16 14:26:20 +0200

    check_three OK

--- src/valid_move.py
+++ src/valid_move.py
@@ -10,7 +10,7 @@ directions = [(-1, -1),
             (1, 1)
             ]
 
-def check_row(board_size, win_len, tab, player):
+def check_column(board_size, win_len, tab, player):
     i = 0
     result = 0
     while i < board_size:
@@ -20,8 +20,12 @@ def check_row(board_size, win_len, tab, player):
                 if j - 1 >= 0:
                     if tab[j - 1][i] == 0:
                         k = 0
-                        while k <= win_len and j < board_size and tab[j][i] == player:
-                            k += 1
+                        lap = 0
+                        while k < win_len and j < board_size and tab[j][i] != -player and lap < 2:
+                            if tab[j][i] == player:
+                                k += 1
+                            else:
+                                lap += 1
                             j += 1
                         if j < board_size:
                             if tab[j][i] == 0 and k == 3:
@@ -45,8 +49,12 @@ def check_diag(board_size, win_len, tab, player):
                 if i - 1 >= 0 and i - 1 - j >= 0:
                     if tab[i - 1][i - 1 - j] == 0:
                             k = 0
-                            while k <= win_len and i < min(board_size, j + board_size) and tab[i][i - j] == player:
-                                k += 1
+                            lap = 0
+                            while k < win_len and i < min(board_size, j + board_size) and tab[i][i - j] != -player and lap < 2:
+                                if tab[i][i - j] == player:
+                                    k += 1
+                                else:
+                                    lap += 1
                                 i += 1
                             if i < board_size and i - j < board_size:
                                 if tab[i][i - j] == 0 and k == 3:
@@ -63,11 +71,11 @@ def check_diag(board_size, win_len, tab, player):
 def check_threes(board, row, col, empty_cell, player):
     tab = np.array(board)
     tab[row][col] = player
-    res = check_row(len(board), 3, tab, player)
-    res += check_row(len(board), 3, tab.transpose(), player)
+    res = check_column(len(board), 3, tab, player)
+    res += check_column(len(board), 3, tab.transpose(), player)
     res += check_diag(len(board), 3, tab, player)
     res += check_diag(len(board), 3, np.rot90(tab, k = 1), player)
-    return res >= 2
+    return (res >= 2)
 
 def check_capture(board, row, col, empty_cell, player):
     for i in range(8):
@@ -88,9 +96,6 @@ def check_capture(board, row, col, empty_cell, player):
 
 def check_valid_move(board, row, col, empty_cell, player):
     if board[row][col] != empty_cell:
-        return False
-    # check if the move creates a capture of player's piece
-    if check_capture(board, row, col, empty_cell, player):
         return False
     # check if the move creates a double-three
     if check_threes(board, row, col, empty_cell, player):
