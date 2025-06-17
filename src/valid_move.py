e1ee7a55a1ab81715738bb3d6590db714a065853
Alban KERLOC'H <alban.kerloh@gmail.fr>
2025-06-17 16:57:24 +0200

    double threes corrected

--- src/valid_move.py
+++ src/valid_move.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def check_column(board_size, win_len, tab, player):
+def check_column(board_size, win_len, tab, player, row, col):
     i = 0
     result = 0
     while i < board_size:
@@ -11,14 +11,18 @@ def check_column(board_size, win_len, tab, player):
                     if tab[j - 1][i] == 0:
                         k = 0
                         lap = 0
+                        valid = 0
                         while k < win_len and j < board_size and tab[j][i] != -player and lap < 2:
+                            # print(f"i,j = {i} {j} => {tab[j][i]}")
+                            if i == row and j == col:
+                                valid = 1
                             if tab[j][i] == player:
                                 k += 1
                             else:
                                 lap += 1
                             j += 1
                         if j < board_size:
-                            if tab[j][i] == 0 and k == 3:
+                            if tab[j][i] == 0 and k == 3 and valid == 1:
                                 result += 1
                     else:
                         j += 1
@@ -29,7 +33,7 @@ def check_column(board_size, win_len, tab, player):
         i += 1
     return result
 
-def check_diag(board_size, win_len, tab, player):
+def check_diag(board_size, win_len, tab, player, row, col):
     result = 0
     j =  1 - board_size
     while j < board_size:
@@ -40,14 +44,18 @@ def check_diag(board_size, win_len, tab, player):
                     if tab[i - 1][i - 1 - j] == 0:
                             k = 0
                             lap = 0
+                            valid = 0
                             while k < win_len and i < min(board_size, j + board_size) and tab[i][i - j] != -player and lap < 2:
+                                # print(f"i,i-j = {i} {i - j} => {tab[i][i - j]}")
+                                if i == row and i - j == col:
+                                    valid = 1
                                 if tab[i][i - j] == player:
                                     k += 1
                                 else:
                                     lap += 1
                                 i += 1
                             if i < board_size and i - j < board_size:
-                                if tab[i][i - j] == 0 and k == 3:
+                                if tab[i][i - j] == 0 and k == 3 and valid == 1:
                                     result += 1
                     else:
                         i += 1
@@ -58,19 +66,23 @@ def check_diag(board_size, win_len, tab, player):
         j += 1
     return result
 
-def check_threes(board, row, col, empty_cell, player):
+def check_threes(board, player, row, col):
     tab = np.array(board)
     tab[row][col] = player
-    res = check_column(len(board), 3, tab, player)
-    res += check_column(len(board), 3, tab.transpose(), player)
-    res += check_diag(len(board), 3, tab, player)
-    res += check_diag(len(board), 3, np.rot90(tab, k = 1), player)
+    res = check_column(len(board), 3, tab, player, col, row)
+    # print(f"Check double threes 1 {res}")
+    res += check_column(len(board), 3, tab.transpose(), player, row, col)
+    # print(f"Check double threes 2 {res}")
+    res += check_diag(len(board), 3, tab, player, row, col)
+    # print(f"Check double threes 3 {res}")
+    res += check_diag(len(board), 3, np.rot90(tab, k = 1), player, len(board) - 1 - col, row)
+    # print(f"Check double threes 4 {res}")
     return (res >= 2)
 
 def check_valid_move(board, row, col, empty_cell, player,  game_rules):
     if board[row][col] != empty_cell:
         return False
     # check if the move creates a double-three
-    if 'Double Three' in game_rules and check_threes(board, row, col, empty_cell, player):
+    if 'Double Three' in game_rules and check_threes(board, player, row, col):
         return False
     return True
